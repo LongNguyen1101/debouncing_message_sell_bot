660cecc15cc56b5b6ca78812abcef64a1ebc86b9
LongNguyen1101 <longnguyenhoang1101.work@gmail.com>
2025-07-28 09:41:54 +0700

    Add cors and requirements

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.router import router
+from fastapi.middleware.cors import CORSMiddleware
 from app.redis_client import r  # để startup listener tự chạy
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cho phép tất cả origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Cho phép tất cả các phương thức (GET, POST, PUT, DELETE, ...)
+    allow_headers=["*"],  # Cho phép tất cả các headers
+)
+
 
 # include toàn bộ router từ router.py
 app.include_router(router, prefix="/api/v1", tags=["debounce-chatbot"])
