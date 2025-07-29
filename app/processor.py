8fddb01d3b208ae57e9a40c3bad18bc822aa50a7
LongNguyen1101 <longnguyenhoang1101.work@gmail.com>
2025-07-29 11:31:25 +0700

    add uuid

--- app/processor.py
+++ app/processor.py
@@ -1,15 +1,20 @@
+import json
 from typing import Any
+from urllib import response
 from app.redis_client import r
 from requests_sse import EventSource
 from dotenv import load_dotenv
 import os
 from requests import RequestException
 import requests
+import uuid
 
 load_dotenv(override=True)
 
 CHATBOT_URL = os.getenv("CHATBOT_URL")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL_PRODUCTION")
+# WEBHOOK_URL = os.getenv("WEBHOOK_URL_TEST")
+mapper_uuid = {}
 
 def send_messages_to_n8n(chat_id: str, content: str) -> Any:
     payload = {
@@ -29,10 +34,11 @@ def send_messages_to_n8n(chat_id: str, content: str) -> Any:
     except requests.RequestException as e:
         return f"[ERROR] Failed sending to n8n: {e}"
 
-def send_messages_to_chatbot(chat_id: str, content: str) -> Any:
+def send_messages_to_chatbot(chat_id: str, content: str, get_uuid: str) -> Any:
     print(f">>>> sending messages: {content}")
-    payload = {"chat_id": chat_id, "user_input": content}
+    payload = {"chat_id": chat_id, "user_input": content, "uuid": get_uuid}
     headers = {"Accept": "text/event-stream"}
+    
     try:
         with EventSource(
             CHATBOT_URL,
@@ -50,13 +56,26 @@ def send_messages_to_chatbot(chat_id: str, content: str) -> Any:
                         break
                     else:
                         status = send_messages_to_n8n(chat_id=chat_id, content=event.data)
-                        print(f"Status send to n8n: {status}")
+                        print(f">>>> Status send to n8n: {status}")
                     
     except RequestException as e:
         print(f"[ERROR] gửi SSE thất bại: {e}")
     finally:
         if source:
             source.close()
+            
+def start_restart_chatbot(chat_id: str) -> Any:
+    try:
+        response = (
+            "Kính chào quý khách đến với cửa hàng ....\n"
+            "Quý khách muốn mua gì ạ."
+        )
+        content = json.dumps({"content": response})
+        status = send_messages_to_n8n(chat_id=chat_id, content=content)
+        print(f">>>> Status send to n8n: {status}")
+                    
+    except RequestException as e:
+        print(f"[ERROR] gửi SSE thất bại: {e}")
 
 def process_messages(chat_id: str):
     key_list = f"messages:{chat_id}"
@@ -64,10 +83,24 @@ def process_messages(chat_id: str):
     if not msgs:
         return
     each = [m for m in msgs]
-    print(f">>>> Process for {chat_id}: {each}")
     
-    content = ". ".join(s for s in each).strip()
+    content = ", ".join(s for s in each).strip()
+    print(f">>>> Process for {chat_id}: {content}")
+    
     r.delete(key_list)
     
-    send_messages_to_chatbot(chat_id, content)
+    get_uuid = mapper_uuid.get(chat_id)
+    if not get_uuid:
+        get_uuid = str(uuid.uuid4())
+        mapper_uuid[chat_id] = get_uuid
+    
+    if content == "/restart" or content == "/start":
+        print(f">>>> Old uuid: {get_uuid}")
+        get_uuid = str(uuid.uuid4())
+        mapper_uuid[chat_id] = get_uuid
+        print(f">>>> New uuid: {get_uuid}")
+        
+        start_restart_chatbot(chat_id)
+    else:
+        send_messages_to_chatbot(chat_id, content, get_uuid)
     
