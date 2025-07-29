b9d99ccea777856cf2e04abc730a9bc38f0a9b41
LongNguyen1101 <longnguyenhoang1101.work@gmail.com>
2025-07-29 15:21:29 +0700

    add lock in redis

--- app/processor.py
+++ app/processor.py
@@ -1,6 +1,7 @@
 import json
 from typing import Any
-from urllib import response
+from telegram.helpers import escape_markdown
+from telegram.constants import ParseMode
 from app.redis_client import r
 from requests_sse import EventSource
 from dotenv import load_dotenv
@@ -17,6 +18,7 @@ WEBHOOK_URL = os.getenv("WEBHOOK_URL_PRODUCTION")
 mapper_uuid = {}
 
 def send_messages_to_n8n(chat_id: str, content: str) -> Any:
+    status = ""
     payload = {
         "chat_id": chat_id,
         "content": content
@@ -30,9 +32,11 @@ def send_messages_to_n8n(chat_id: str, content: str) -> Any:
         resp = requests.post(WEBHOOK_URL, json=payload, headers=headers, timeout=10)
         resp.raise_for_status()
         
-        return "OK"
+        status = "OK"
     except requests.RequestException as e:
-        return f"[ERROR] Failed sending to n8n: {e}"
+        status = f"[ERROR] Failed sending to n8n: {e}"
+    finally:
+        return status
 
 def send_messages_to_chatbot(chat_id: str, content: str, get_uuid: str) -> Any:
     print(f">>>> sending messages: {content}")
@@ -77,30 +81,83 @@ def start_restart_chatbot(chat_id: str) -> Any:
     except RequestException as e:
         print(f"[ERROR] gửi SSE thất bại: {e}")
 
+# def process_messages(chat_id: str):
+#     key_list = f"messages:{chat_id}"
+#     msgs = r.lrange(key_list, 0, -1)
+    
+#     if not msgs:
+#         return
+#     each = [m for m in msgs]
+#     content = ", ".join(s for s in each).strip()
+#     r.delete(key_list)
+    
+#     get_uuid = mapper_uuid.get(chat_id)
+#     if not get_uuid:
+#         get_uuid = str(uuid.uuid4())
+#         mapper_uuid[chat_id] = get_uuid
+        
+#     print(f">>>> Process for {chat_id}: {content} | uuid: {get_uuid}")
+    
+#     if content == "/restart" or content == "/start":
+#         print(f">>>> Old uuid: {get_uuid}")
+#         get_uuid = str(uuid.uuid4())
+#         mapper_uuid[chat_id] = get_uuid
+#         print(f">>>> New uuid: {get_uuid}")
+        
+#         start_restart_chatbot(chat_id)
+#     else:
+#         send_messages_to_chatbot(chat_id, content, get_uuid)
+    
+# Lua script release lock an toàn
+RELEASE_SCRIPT = """
+if redis.call("get", KEYS[1]) == ARGV[1] then
+  return redis.call("del", KEYS[1])
+else
+  return 0
+end
+"""
+release_lock = r.register_script(RELEASE_SCRIPT)
+
+def acquire_lock(lock_key: str, ttl_ms: int = 6000) -> str | None:
+    token = str(uuid.uuid4())
+    ok = r.set(lock_key, token, nx=True, px=ttl_ms)
+    return token if ok else None
+
+def free_lock(lock_key: str, token: str) -> None:
+    try:
+        release_lock(keys=[lock_key], args=[token])
+    except Exception as e:
+        print(f"[WARN] free_lock exception: {e}")
+
 def process_messages(chat_id: str):
-    key_list = f"messages:{chat_id}"
-    msgs = r.lrange(key_list, 0, -1)
-    
-    if not msgs:
+    lock_key = f"debounce_lock:{chat_id}"
+    token = acquire_lock(lock_key)
+    if not token:
+        print(f">>>> Skip process_messages({chat_id}): already locked")
         return
-    each = [m for m in msgs]
-    content = ", ".join(s for s in each).strip()
-    r.delete(key_list)
-    
-    get_uuid = mapper_uuid.get(chat_id)
-    if not get_uuid:
-        get_uuid = str(uuid.uuid4())
-        mapper_uuid[chat_id] = get_uuid
-        
-    print(f">>>> Process for {chat_id}: {content} | uuid: {get_uuid}")
-    
-    if content == "/restart" or content == "/start":
-        print(f">>>> Old uuid: {get_uuid}")
-        get_uuid = str(uuid.uuid4())
-        mapper_uuid[chat_id] = get_uuid
-        print(f">>>> New uuid: {get_uuid}")
-        
-        start_restart_chatbot(chat_id)
-    else:
-        send_messages_to_chatbot(chat_id, content, get_uuid)
-    
+
+    try:
+        key_list = f"messages:{chat_id}"
+        msgs = r.lrange(key_list, 0, -1)
+        if not msgs:
+            return
+        content = ", ".join(m for m in msgs).strip()
+        r.delete(key_list)
+
+        get_uuid = mapper_uuid.get(chat_id)
+        if not get_uuid:
+            get_uuid = str(uuid.uuid4())
+            mapper_uuid[chat_id] = get_uuid
+
+        print(f">>>> Process for {chat_id}: {content} | uuid: {get_uuid}")
+
+        if content in ("/restart", "/start"):
+            get_uuid = str(uuid.uuid4())
+            mapper_uuid[chat_id] = get_uuid
+            print(f">>>> New uuid: {get_uuid}")
+            start_restart_chatbot(chat_id)
+        else:
+            send_messages_to_chatbot(chat_id, content, get_uuid)
+
+    finally:
+        free_lock(lock_key, token)
